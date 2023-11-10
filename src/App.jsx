14b21ac9af82d2938f1a1e8278d0fe88536f1530
Paradox Alex <sherstyukalex@gmail.com>
2023-11-10 23:01:52 +0200

    button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { Button } from './components/button'
 import './App.css'
 
 function App() {
-
-
-  return (
-    <>
+  return <>
      <h1>Hello</h1>
+     <Button>Hello </Button>
+     <h2>Button Size</h2>
+     <div className='button-group'>
+     <Button size='lg'>Hello </Button>
+     <Button size='md'>Hello </Button>
+     <Button size='sm'>Hello </Button>
+     </div>
+     <h2>Button Color</h2>
+     <div className='button-group'>
+     <Button size='sm' color='neutral'>neutral </Button>
+     <Button color='primary'>primary </Button>
+     <Button color='success'>success </Button>
+     <Button color='danger'>danger </Button>
+     <Button color='warning'>warning </Button>
+     </div>
+     <h2>Button Color Variant</h2>
+     <div className='button-group'>
+     <Button color='primary' variant='soft'>primary soft</Button>
+     <Button color='success' variant='soft'>primary soft</Button>
+     <Button color='success' variant='outlined'>success outlined</Button>
+     <Button color='primary' variant='outlined'>primary outlined</Button>
+     <Button color='danger' variant='soft'>danger soft</Button>
+     <Button color='danger' variant='outlined'>danger outlined</Button>
+     <Button color='warning' variant='soft'>warning soft</Button>
+     <Button color='warning' variant='plain'>warning plain</Button>
+     </div>
+     <h2>Disabled button</h2>
+     <div className='button-group'>
+     <Button color='warning' variant='plain' disabled={true}>warning plain</Button>
+     <Button color='warning' variant='outlined' disabled={true}>warning outlined</Button>
+     <Button color='warning' disabled>warning </Button>
+     </div>
     </>
-  )
+  
 }
 
 export default App
---
+++ src/storage/const.js
@@ -0,0 +1,29 @@
+export const STORAGE_STYLE = {};
+export const CACHE_TABLE = {};
+export const COLOR = {
+  neutral: {
+    defaultColor: "var(--local-background-color-neutral)",
+    softColor: "var(--local-background-color-neutral-soft)",
+    softColorHover: "var(--local-background-color-neutral-soft-hover)",
+  },
+  primary: {
+    defaultColor: "var(--local-background-color-primary)",
+    softColor: "var(--local-background-color-primary-soft)",
+    softColorHover: "var(--local-background-color-primary-soft-hover)",
+  },
+  success: {
+    defaultColor: "var(--local-background-color-success)",
+    softColor: "var(--local-background-color-success-soft)",
+    softColorHover: "var(--local-background-color-success-soft-hover)",
+  },
+  danger: {
+    defaultColor: "var(--local-background-color-danger)",
+    softColor: "var(--local-background-color-danger-soft)",
+    softColorHover: "var(--local-background-color-danger-soft-hover)",
+  },
+  warning: {
+    defaultColor: "var(--local-background-color-warning)",
+    softColor: "var(--local-background-color-warning-soft)",
+    softColorHover: "var(--local-background-color-warning-soft-hover)",
+  },
+};
