0d314431f33aafd81bcbf54f90ec4e6c9c2a4ecd
Paradox Alex <sherstyukalex@gmail.com>
2023-11-24 22:47:33 +0200

    dz39

--- src/sections/main/Main.jsx
+++ src/sections/main/Main.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import { Post } from "../../componets/post"
 import { useIdUserContext } from "../../hooks/useIdUserContext"
+import { useAuthContext } from "../../hooks/useAuthContext"
 import { getPost } from "../../services/getData"
 
 export const Main = () => {
   const [posts, setPosts] = useState([])
+  const {isAuth} = useAuthContext()
   const {id} = useIdUserContext()
-  
+
   useEffect(()=>{
    if(id){
      (async()=>{
@@ -19,17 +21,19 @@ export const Main = () => {
   return (
     <main className="flex justify-center max-w-5xl mx-auto mb-16 px-10">
 
-      {posts.length ===0 && <span>Please Login</span>}
-      {posts.length !==0 && <div className="grid gap-x-8 gap-y-16 lg:grid-cols-3">{posts.map((post)=>{
-        return <Post  key = {post.id} 
-                      title = {post.title}
-                      body = {post.body} 
-                      tags = {post.tags} 
-                      isAuthor = {post.userId == id}
-                      reactions = {post.reactions}
-                      id={post.userId}
-                />
-      })}</div>}
+      {!isAuth && <span>Please Login</span>}
+      {isAuth && <div className="grid gap-x-8 gap-y-16 lg:grid-cols-3">
+                  {posts.map((post)=>{
+                  return <Post  key = {post.id} 
+                                title = {post.title}
+                                body = {post.body} 
+                                tags = {post.tags} 
+                                isAuthor = {post.userId == id}
+                                reactions = {post.reactions}
+                                id={post.userId}
+                          />
+                  })}
+                  </div>}
     </main>
   )
 }
